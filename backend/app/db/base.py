b8b84de503946386023a1d2a6b2a8ba6c08103b6
KEVIN WANSI <142133556+wkevin240@users.noreply.github.com>
2026-09-10 00:44:47 +0100

    chore: use SQLAlchemy 2 declarative base API

--- backend/app/db/base.py
+++ backend/app/db/base.py
@@ -1,8 +1,9 @@
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import declared_attr
-from datetime import datetime
-from sqlalchemy import Column, DateTime, String
 import uuid
+from datetime import datetime
+
+from sqlalchemy import Column, DateTime, String
+from sqlalchemy.orm import declarative_base, declared_attr
+
 
 class CustomBase:
     id = Column(String, primary_key=True, default=lambda: str(uuid.uuid4()))
@@ -12,5 +13,6 @@ class CustomBase:
     @declared_attr
     def __tablename__(cls):
         return cls.__name__.lower()
+
 
 Base = declarative_base(cls=CustomBase)
